69db2628da810668ada3a24f95a86f01aac1dbce
Ivan Tolkunov <tolkunov.ivan.s@gmail.com>
2024-01-01 19:42:14 -0800

    Remove secret key

--- omori/settings.py
+++ omori/settings.py
@@ -21,12 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-d$ee_f*qh7n%pfw^+)03c$5$6lj*r--3j85wn#g#s837g!8k=t'
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['omori.tolkunov.dev']
+ALLOWED_HOSTS = ['omori.tolkunov.dev', 'localhost']
 
 CSRF_TRUSTED_ORIGINS = ['https://omori.tolkunov.dev']
 
