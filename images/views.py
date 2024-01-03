a92b7a150e3f05945432e82228a6330c49cb714f
Ivan Tolkunov <tolkunov.ivan.s@gmail.com>
2024-01-02 23:01:39 -0800

    Added error handler

--- images/views.py
+++ images/views.py
@@ -7,12 +7,13 @@ from django.core.files.storage import default_storage
 from pathlib import Path
 from datetime import datetime
 from django.templatetags.static import static
+from django.contrib import messages
 
 
 from omori import settings
 
 def index(request):
-    user = request.GET.get('user')
+    user = request.GET.get("user")
     if not user:
         return HttpResponse("User not found", status=404)
     
@@ -21,31 +22,35 @@ def index(request):
 
     return render(request, "images/index.html", {
         "files": files,
-        "latest_file": user + '/' + files[0] if files else None,
+        "latest_file": user + "/" + files[0] if files else None,
         "user": user
     })
 
 def upload_img(request):
-    file = request.FILES.get('image')
-    receiver = request.POST.get('receiver')
-    user = request.POST.get('user')
-    now = datetime.now()
-    timestamp = now.strftime('%Y%m%d_%H_%M_%S')
-    _, file_extension = os.path.splitext(file.name)
-    file_name = Path(settings.MEDIA_ROOT) / receiver / (timestamp + file_extension)
-    default_storage.save(file_name, file)
-
-    base_url = reverse('index')
-    query_string = urlencode({'user': user})
-    url = '{}?{}'.format(base_url, query_string)
-
-    return redirect(url)
+    try:
+        file = request.FILES.get("image")
+        receiver = request.POST.get("receiver")
+        user = request.POST.get("user")
+        now = datetime.now()
+        timestamp = now.strftime("%Y%m%d_%H_%M_%S")
+        _, file_extension = os.path.splitext(file.name)
+        if file_extension.lower() not in [".png", ".jpeg", ".heic", ".jpg", ".gif"]:
+            raise ValueError("Unsupported file extension")
+        file_name = Path(settings.MEDIA_ROOT) / receiver / (timestamp + file_extension)
+        default_storage.save(file_name, file)
+    except:
+        messages.add_message(request, messages.ERROR, "Upload failed")
+    finally:
+        base_url = reverse("index")
+        query_string = urlencode({"user": user})
+        url = "{}?{}".format(base_url, query_string)
+        return redirect(url)
 
 def get_img(request):
-    user = request.GET.get('user')
+    user = request.GET.get("user")
     folder = Path(settings.MEDIA_ROOT) / user
     files = sorted(os.listdir(folder), reverse=True)
     if len(files) < 1:
-        image_url = static('img/love-you.gif')
+        image_url = static("img/love-you.gif")
         return redirect(image_url)
-    return redirect(settings.MEDIA_URL + user + '/' + files[0])
+    return redirect(settings.MEDIA_URL + user + "/" + files[0])
